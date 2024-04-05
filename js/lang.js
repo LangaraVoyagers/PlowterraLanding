e0df777e7a07083d53b1321c190d1bab164f7d53
BryanCaldeira <bryancaldeira22@gmail.com>
2024-04-04 19:33:11 -0700

    feat: theme and lang support added

---
+++ js/lang.js
@@ -0,0 +1,226 @@
+const lang = {
+  en: { 
+    navbarLinks: [
+      "Benifits",
+      "Features",
+      "Pricing" ,
+      "Team",
+    ],
+    loginBtn: "Login",
+    section1: {
+      mainHeading: "Elevate your farm's efficiency with seamless Payroll Management",
+      tagline: "Experience the future of farm management with our intuitive web app. From picker management to daily harvest logs, we've got you covered every step of the way.",
+      demoBtn: "Request Demo"
+    },
+    section2: {
+      mainHeading: `Benefits of using <span class="txt__highlight"> Plowterra </span> for farm management`,
+      tagline: "Ditch the spreadsheets and other manual methods of logging your daily harvest and doing payroll calculations.",
+      table: [
+        {
+          tagline: "Payroll calculation accuracy",
+          text: "Record picker's daily production and generate payroll automatically."
+        },
+        {
+          tagline: "Increase in lifetime value",
+          text: "Track a variety of metrics to help farm managers make informed business decisions, such as customer lifetime value (CLV)."
+        },
+        {
+          tagline: "Track where you are spending",
+          text: "Log the harvest statistics and deductions with multiple time frames. Review the season report anytime to have a detailed check."
+        }
+      ]
+    },
+    section3: {
+      mainHeading: `Have a look at some <span class="txt__highlight"> amazing features </span> of Plowterra`,
+      features : [{
+        mainHeading: "Picker Management",
+        tagline: "Whether you have a team of 10 or 200, our shared team inboxes keep everyone on the same page and in the loop.",
+        list: [
+          "Add pickers in a short seconds",
+          "Keep pickers informed via SMS of daily production",
+          "Leverage individual profile for multiple occassions"
+        ]
+      },
+      {
+        mainHeading: "Harvest Season Management",
+        tagline: "An overview of the seasonal statement will be delivered for a boarder perspective that empower you to make smarter decisions.",
+        list: [
+          "Set up seasonal report",
+          "Retrospect historical harvest data",
+          "Switch between active and closed status"
+        ]
+      },
+      {
+        mainHeading: "Daily Harvest Log",
+        tagline: "Whether its individual production of pickers or daily production of the farm, log every detail of the product efficiently.",
+        list: [
+          "Log daily harvest details",
+          "Ensure data accuracy with correction notes",
+          "Check logs with flexible filters"
+        ]
+      },
+      {
+        mainHeading: "Payroll Management",
+        tagline: "Generate payroll seamless no matter what type of timeframe you choose, ensuring efficient distribution to workers.",
+        list: [
+          "Generate preferred timeframe payroll immediately",
+          "Run the specific picker's payroll timely"
+        ]
+      }]
+    },
+    section4: {
+      mainHeading: `Plans that <span class="txt__highlight"> your scale </span>`,
+      tagline: "Experience the business plan at no cost for 30 days. Book a demo and activate your complimentary trial today!",
+      plans: [{
+        mainHeading: "Basic Plan",
+        tagline: "Ideal plan for small farms",
+        durationTxt: "per month",
+        list: [
+          "1 active season allowed",
+          "Up to 19 pickers",
+          "Picker notifications via SMS",
+          "Analytics dashboard without customization",
+          "24/7 customer support"
+        ],
+        btn: "Get Started"
+      },
+      {
+        mainHeading: `Premium Plan <span class="badge__tag__popular">Popular</span>`,
+        tagline: "Advanced features and analytics",
+        durationTxt: "per month",
+        list: [
+          "All features from the Basic Plan",
+          "Unlimited number of pickers",
+          "Unlimited active season manage",
+          "Customizable dashboard options",
+          "24/7 customer support"
+        ],
+        btn: "Get Started"
+      }]
+    },
+    section5: {
+      mainHeading: `The <span class="txt__highlight"> team </span> behind Plowterra`,
+      tagline: "Meet the team members created this awesome tool",
+      designersTag: "Designers",
+      developersTag: "Developers"
+    },
+    footer: {
+      tagline: "Elevate your farm's efficiency with seamless Payroll Management",
+      btn: "Request Demo",
+      copyRightTxt: `&copy; ${ new Date().getFullYear() } Plowterra. All rights reserved.`
+    }
+  },
+  es:  { 
+    navbarLinks: [
+      "Beneficios",
+      "Características",
+      "Precios",
+      "Equipo"
+    ],
+    loginBtn: "Acceso",
+    section1: {
+      mainHeading: "Eleve la eficiencia de su granja con una gestión de nómina perfecta",
+      tagline: "Experimente el futuro de la gestión agrícola con nuestra intuitiva aplicación web. Desde la gestión de recolectores hasta los registros diarios de cosecha, lo cubrimos en cada paso del camino.",
+      demoBtn: "Solicitar demostración"
+    },
+    section2: {
+      mainHeading: `Beneficios de utilizar <span class="txt__highlight"> Plowterra </span> para la gestión agrícola`,
+      tagline: "Deshágase de las hojas de cálculo y otros métodos manuales para registrar su cosecha diaria y realizar cálculos de nómina.",
+      table: [
+        {
+          tagline: "Precisión del cálculo de la nómina",
+          text: "Registre la producción diaria del selector y genere la nómina automáticamente."
+        },
+        {
+          tagline: "Aumento del valor de por vida",
+          text: "Realice un seguimiento de una variedad de métricas para ayudar a los administradores agrícolas a tomar decisiones comerciales informadas, como el valor de vida del cliente (CLV)."
+        },
+        {
+          tagline: "Realice un seguimiento de dónde está gastando",
+          text: "Registre las estadísticas de cosecha y las deducciones con múltiples períodos de tiempo. Revise el informe de temporada en cualquier momento para realizar una verificación detallada."
+        }
+      ]
+    },
+    section3: {
+      mainHeading: `Eche un vistazo a algunas <span class="txt__highlight"> características sorprendentes </span> de Plowterra`,
+      features : [{
+        mainHeading: "Gestión de selectores",
+        tagline: "Ya sea que tenga un equipo de 10 o 200 personas, nuestras bandejas de entrada de equipo compartidas mantienen a todos en sintonía y informados.",
+        list: [
+          "Agregue selectores en unos segundos",
+          "Mantenga a los recolectores informados vía SMS de la producción diaria",
+          "Aproveche el perfil individual para múltiples ocasiones"
+        ]
+      },
+      {
+        mainHeading: "Manejo de la temporada de cosecha",
+        tagline: "Se entregará una descripción general de la declaración estacional para obtener una perspectiva interna que le permitirá tomar decisiones más inteligentes.",
+        list: [
+          "Configurar informe estacional",
+          "Retrospectiva de datos históricos de cosecha",
+          "Cambiar entre estado activo y cerrado"
+        ]
+      },
+      {
+        mainHeading: "Registro diario de cosecha",
+        tagline: "Ya sea la producción individual de los recolectores o la producción diaria de la granja, registre cada detalle del producto de manera eficiente.",
+        list: [
+          "Registrar detalles de cosecha diaria",
+          "Garantice la precisión de los datos con notas de corrección",
+          "Consultar registros con filtros flexibles"
+        ]
+      },
+      {
+        mainHeading: "Gestión de nóminas",
+        tagline: "Genere nóminas sin problemas sin importar el tipo de período que elija, garantizando una distribución eficiente a los trabajadores.",
+        list: [
+          "Genere nóminas con plazos preferidos de inmediato",
+          "Ejecute la nómina del seleccionador específico de manera oportuna"
+        ]
+      }]
+    },
+    section4: {
+      mainHeading: `Planes que <span class="txt__highlight"> tu escala </span>`,
+      tagline: "Experimenta el plan de negocios sin costo durante 30 días. ¡Reserve una demostración y active su prueba gratuita hoy!",
+      plans: [{
+        mainHeading: "Plan Básico",
+        tagline: "Plan ideal para pequeñas explotaciones.",
+        durationTxt: "por mes",
+        list: [
+          "1 temporada activa permitida",
+          "Hasta 19 recogedoras",
+          "Notificaciones del selector vía SMS",
+          "Panel de análisis sin personalización",
+          "Atención al cliente 24 horas al día, 7 días a la semana"
+        ],
+        btn: "Empezar"
+      },
+      {
+        mainHeading: `Plan Premium <span class="badge__tag__popular">Popular</span>`,
+        tagline: "Funciones y análisis avanzados",
+        durationTxt: "por mes",
+        list: [
+          "Todas las funciones del Plan Básico",
+          "Número ilimitado de recolectores",
+          "Gestión ilimitada de temporada activa",
+          "Opciones de panel personalizables",
+          "Atención al cliente 24 horas al día, 7 días a la semana"
+        ],
+        btn: "Empezar"
+      }]
+    },
+    section5: {
+      mainHeading: `El <span class="txt__highlight"> equipo </span> detrás de Plowterra`,
+      tagline: "Conozca a los miembros del equipo que crearon esta increíble herramienta",
+      designersTag: "Diseñadoras",
+      developersTag: "Desarrolladoras"
+    },
+    footer: {
+      tagline: "Eleve la eficiencia de su granja con una gestión de nómina perfecta",
+      btn: "Solicitar demostración",
+      copyRightTxt: `&copy; ${ new Date().getFullYear() } Plowterra. Reservados todos los derechos.`
+    }
+  }
+};
+
+export default lang;
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+import lang from "./js/lang.js";
 
+const settingsBtn = document.getElementById("settingsBtn");
+const settingsMenu = document.getElementById("settingsMenu");
 const accuracyTxt = document.getElementById("accuracyTxt");
 const valueTxt = document.getElementById("valueTxt");
 const spendingTxt = document.getElementById("spendingTxt");
+const langOption = document.getElementById("langOption");
 
 let accuracy = 90
 let value = 0;
@@ -52,36 +56,73 @@ const formSuccessPopup = () => {
   });
 }
 
+const changeLang = (locale="es") => {
+  const langObj = lang[locale];
+  const navMenuList = document.querySelector(".navbar__menu__list");
+  const loginBtn = document.querySelector("#loginBtn").querySelector("span");
+  const section1 = document.querySelector(".main__section1");
+  const section2 = document.querySelector(".main__section2");
+  const section3 = document.querySelector(".main__section3");
+  const section4 = document.querySelector(".main__section4");
+  const section5 = document.querySelector(".main__section5");
+  const footer = document.querySelector("footer");
+  const copyRightTxt = document.getElementById("copyRightTxt");
+  
+  navMenuList.querySelectorAll("a").forEach((item, idx) => {
+    item.innerText = langObj?.navbarLinks[idx];
+  });
+  
+  loginBtn.innerText = langObj?.loginBtn;
+  section1.querySelector("h1").innerText = langObj?.section1.mainHeading;
+  section1.querySelector("p").innerText = langObj?.section1.tagline;
+  section1.querySelector("button").querySelector("span").innerText = langObj?.section1?.demoBtn;
+  
+  section2.querySelector("h2").innerHTML = langObj?.section2?.mainHeading;
+  section2.querySelector(".section2__content__tagline").innerText = langObj?.section2?.tagline;
+  section2.querySelectorAll(".section2__col").forEach((item, idx) => {
+    item.querySelector(".section2__col__tagline").innerText = langObj?.section2?.table[idx]?.tagline;
+    item.querySelector(".section2__col__txt").innerText = langObj?.section2?.table[idx]?.text;
+  });
+  
+  section3.querySelector("h2").innerHTML = langObj?.section3?.mainHeading;
+  let section3ColCounter = 0;
+  section3.querySelectorAll(".section3__col")?.forEach((item1) => {
+    if (item1.querySelector(".section3__col__heading")) {
+      item1.querySelector(".section3__col__heading").innerText = langObj?.section3?.features[section3ColCounter]?.mainHeading;
+      item1.querySelector(".section3__col__tagline").innerText = langObj?.section3?.features[section3ColCounter]?.tagline;
+      item1.querySelector(".section3__col__list").querySelectorAll("li").forEach((item2, idx2) => {
+        item2.innerText = langObj?.section3?.features[section3ColCounter]?.list[idx2];
+      });
+      section3ColCounter++;
+    }
+  });
 
-const onClickDemo = (plan) => {
-  popupS.modal({
-    content: `
-    <form action="https://formsubmit.co/outsafebc@gmail.com" method="POST">
-      <ul class="form-style-1">
-        <li><label>Full Name <span class="required">*</span></label><input type="text" name="First Name" class="field-divided" placeholder="First" required/> <input type="text" name="Last Name" class="field-divided" placeholder="Last" /></li>
-        <li>
-          <label>Email <span class="required">*</span></label>
-          <input type="email" name="Email" class="field-long" placeholder="Email" required/>
-        </li>
-        <li>
-          <label>Plan Type</label>
-          <select name="Plan Type" class="field-select">
-            <option value="Demo" ${ plan === "demo" ? "selected" : "" }>Demo</option>
-            <option value="Basic" ${ plan === "basic" ? "selected" : "" }>Basic</option>
-            <option value="Premium" ${ plan === "premium" ? "selected" : "" }>Premium</option>
-          </select>
-        </li>
-        <li>
-          <label>Your Message <span class="required">*</span></label>
-          <textarea name="Message" id="field5" class="field-long field-textarea" required></textarea>
-          <input type="hidden" name="_next" value="https://about.plowterra.com/?form-success=true">
-          <input type="hidden" name="_subject" value="Plowterra Application Demo">
-        </li>
-        <li>
-          <input type="submit" value="Submit" />
-        </li>
-      </ul>
-    </form>`
+  section4.querySelector("h2").innerHTML = langObj?.section4?.mainHeading;
+  section4.querySelector(".section4__tagline").innerText = langObj?.section4?.tagline;
+
+  section4.querySelectorAll(".section4__col").forEach((item1, idx1) => {
+    item1.querySelector("h4").innerHTML = langObj?.section4?.plans[idx1]?.mainHeading;
+    item1.querySelector(".duration__tag").innerText = langObj?.section4?.plans[idx1]?.durationTxt;
+    item1.querySelector(".plan__tagline").innerText = langObj?.section4?.plans[idx1]?.tagline;
+
+    item1.querySelector(".section4__list").querySelectorAll("li")?.forEach((item2, idx2) => {
+      item2.innerText = langObj?.section4?.plans[idx1]?.list[idx2];
+    })
+
+    item1.querySelector("button").innerText = langObj?.section4?.plans[idx1]?.btn;
+  });
+
+  section5.querySelector("h2").innerHTML = langObj?.section5.mainHeading;
+  section5.querySelector(".section5__col__tagline").innerText = langObj?.section5.tagline;
+  section5.querySelector("#designersTag").innerText = langObj?.section5.designersTag;
+  section5.querySelector("#developersTag").innerText = langObj?.section5.developersTag;
+
+  footer.querySelector(".footer__tagline").querySelector("p").innerText = langObj?.footer?.tagline;
+  footer.querySelector("button").querySelector("span").innerText = langObj?.footer?.btn;
+  copyRightTxt.innerHTML = langObj?.footer?.copyRightTxt;
+
+  footer.querySelector(".footer__links").querySelectorAll("a").forEach((item, idx) => {
+    item.innerText = langObj?.navbarLinks[idx];
   });
 }
 
@@ -91,5 +132,15 @@ observer.observe(spendingTxt);
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const formSuccess = urlParams.get('form-success')
+const formSuccess = urlParams.get("form-success");
+const langPref = urlParams.get("lang");
 if (formSuccess === "true") formSuccessPopup();
+
+changeLang(langPref === "es" ? "es" : "en");
+langOption.innerText = langPref === "es" ? "EN" : "ES";
+langOption.href = `https://plowterra.com/?lang=${ langPref === "es" ? "en" : "es" }`;
+
+settingsBtn.addEventListener("click", () => {
+  const menuStyle = settingsMenu.style;
+  menuStyle.display = (menuStyle.display === "block") ? "none" : "block";
+});
